a7c0bb03402bf00ebcd63d945a7c9eaa649f6fd7
12243722 <12243722@qq.com>
2021-08-29 22:46:24 +0800

    new

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from "react";
+
+function App(props) {
+  return (
+    <div className="App" style={{ height: "100%" }}>
+      {props.children}
+    </div>
+  );
+}
+
+export default App;
